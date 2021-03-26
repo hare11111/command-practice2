1cafa6c9e467ca0532c586d1ae310690dffd2ada
onishi haruya <h.oonishi.sys20@morijyobi.ac.jp>
2021-03-26 11:16:50 +0900

    main.py修正
    BMIが正しく計算されるように修正。また、BMIが二回表示されるように修正

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import calc_bmi
 
 print("BMI計算アプリ")
-b = int(input("体重:"))
-h = int(input("身長:"))
+b = int(input("体重(kg):"))
+h = int(input("身長(cm):")) /100
+print(f"BMI:{calc_bmi.get_bmi(b,h)}")
 print(f"BMI:{calc_bmi.get_bmi(b,h)}")
